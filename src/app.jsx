b76fc202772e9717de92f9f2d06570af4ef8a1d5
Glitch (cut-woolly-brand) <none>
2025-05-20 17:57:26 +0000

    🙏🖖 Checkpoint ./src/components/toolsBar.jsx:76642879/1472 ./src/app.jsx:76642879/239

--- src/app.jsx
+++ src/app.jsx
@@ -6,11 +6,13 @@ import "./styles/styles.css";
 import PageRouter from "./components/router.jsx";
 import Toggler from './components/hemisphereToggler.jsx';
 import Seo from './components/seo.jsx';
+import ToolsBar from './components/toolsBar.jsx';
 
 export const DataContext = createContext(null);
 
 export default function Home() {
   const [hemisphere, setHemisphere] = useState("N");
+  const [fov, setFov] = useState('0.7');
   const [loading, setLoading] = useState(true);
   const [topPorFovNorth, setTopPorFovNorth] = useState({});
   const [topPorFovSouth, setTopPorFovSouth] = useState({});
@@ -40,6 +42,7 @@ export default function Home() {
           <div className="content">
             <Toggler selected={hemisphere} onSelect={setHemisphere} />
             <PageRouter />
+            <ToolsBar hemisphereSelected={hemisphere} onHemispehereSelected={setHemisphere} fovSelected={fov} onFovSelected={setFov} />
           </div>
         </main>
       </Router>
---
+++ src/components/toolsBar.jsx
@@ -0,0 +1,21 @@
+import { useState, useContext } from "react";
+import { Link } from "wouter";
+import { DataContext } from '../app.jsx';
+
+export default function ToolsBar({ hemisphereSelected, onHemisphereSelect, fovSelected, onFovSelected }) {
+  return (
+    <section className="toolsBarContainer">
+      <section className="hemisphereSelector">
+        <button>Norte</button>
+        <button>Sur</button>
+      </section>
+      
+      <section className="fovSelector">
+        <button>0.7</button>
+        <button>1.5</button>
+        <button>3.5</button>
+        <button>7.0</button>
+      </section>
+    </section>
+  )
+}
