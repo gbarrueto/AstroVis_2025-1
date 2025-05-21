030ccccbc522d70561b18992cabdbb91c64e84e3
Glitch (cut-woolly-brand) <none>
2025-05-21 13:21:14 +0000

    👆🍝 Checkpoint ./src/components/toolsBar.jsx:76642879/160 ./src/styles/toolsBarStyle.css:76642879/91

--- src/components/toolsBar.jsx
+++ src/components/toolsBar.jsx
@@ -14,7 +14,8 @@ const fovColors = {
 export default function ToolsBar({ hemisphereSelected, onHemisphereSelected, fovSelected, onFovSelected }) {
   
   useEffect(() => {
-    document.getElementsByClassName()
+    const activeFovSelector = document.getElementsByClassName('activeFovSelector')[0];
+    activeFovSelector.style.backgroundColor = fovColors[fovSelected];
   }, [fovSelected])
   
   return (
@@ -38,28 +39,28 @@ export default function ToolsBar({ hemisphereSelected, onHemisphereSelected, fov
         
         <Link
           to={`/${hemisphereSelected}/0.7`}
-          className={`fovSelector ${fovSelected === "0.7" ? "activeFovSelector" : ""}`}
+          className={`fovSelector ${fovSelected === "0.7" ? "activeFovSelector" : "resetFov"}`}
           onClick={() => onFovSelected("0.7")}
         > 0.7
         </Link>
         
         <Link
           to={`/${hemisphereSelected}/1.5`}
-          className={`fovSelector ${fovSelected === "1.5" ? "activeFovSelector" : ""}`}
+          className={`fovSelector ${fovSelected === "1.5" ? "activeFovSelector" : "resetFov"}`}
           onClick={() => onFovSelected("1.5")}
         > 1.5
         </Link>
         
         <Link
           to={`/${hemisphereSelected}/3.5`}
-          className={`fovSelector ${fovSelected === "3.5" ? "activeFovSelector" : ""}`}
+          className={`fovSelector ${fovSelected === "3.5" ? "activeFovSelector" : "resetFov"}`}
           onClick={() => onFovSelected("3.5")}
         > 3.5
         </Link>
         
         <Link
           to={`/${hemisphereSelected}/7.0`}
-          className={`fovSelector ${fovSelected === "7.0" ? "activeFovSelector" : ""}`}
+          className={`fovSelector ${fovSelected === "7.0" ? "activeFovSelector" : "resetFov"}`}
           onClick={() => onFovSelected("7.0")}
         > 1.5
         </Link>
