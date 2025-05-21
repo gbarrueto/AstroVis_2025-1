a3de6b916b92656dd81aa920f45da39f749f4bd0
Glitch (cut-woolly-brand) <none>
2025-05-21 01:17:10 +0000

    😀🎹 Checkpoint ./src/components/rightSidePanel.jsx:76642879/742

--- src/components/rightSidePanel.jsx
+++ src/components/rightSidePanel.jsx
@@ -1,11 +1,39 @@
-import ToolsBar from './toolsBar.jsx';
+import { useContext } from 'react';
+import ToolsBar from './toolsBar';
+import Modal from './modal';
+import { Context } from "../app.jsx";
 import '../styles/rightSidePanelStyle.css';
 
+
 export default function RightPanel({ hemisphereSelected, setHemisphereSelected, fovSelected, setFovSelected }) {
+  const {
+    loading,
+    setLoading,
+    topPorFovNorth,
+    setTopPorFovNorth,
+    topPorFovSouth,
+    setTopPorFovSouth,
+    listasPorFovNorth,
+    setListasPorFovNorth,
+    listasPorFovSouth,
+    setListasPorFovSouth,
+    isModalOpen,
+    setIsModalOpen, // Estado del modal
+    selectedObject,
+    setSelectedObject, // Objeto seleccionado
+  } = useContext(Context);
+  
+  
   return (
     <aside className="rightPanel">
       
       <ToolsBar hemisphereSelected={hemisphereSelected} onHemisphereSelected={setHemisphereSelected} fovSelected={fovSelected} onFovSelected={setFovSelected} />
+      
+      <Modal
+        isOpen={isModalOpen} // Determina si el modal está abierto
+        objectData={selectedObject} // Pasa el objeto seleccionado al modal
+        onClose={() => setIsModalOpen(false)} // Cierra el modal
+      />
       
     </aside>
   )
