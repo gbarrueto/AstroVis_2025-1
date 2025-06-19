99b4a598f285b135a0ce8cd860f7e4d1337d84b8
Glitch (infovis-g12-e2) <none>
2025-06-19 15:35:30 +0000

    📌☄️ Checkpoint ./src/Arduino/config.js:77733200/182 ./public/placa.html:77733200/626 ./public/index.html:77733200/12 ./public/config.js:77733200/12 ./src/components/protObject.jsx:77733200/586

---
+++ public/config.js
@@ -0,0 +1,15 @@
+Protobject.setProduction(true)
+Protobject.initialize(
+	[
+		{ 
+			name: "Servo",
+			page: "placa.html",
+		},
+		{ 
+			name: "Knob",
+			page: "index.html",
+			main: true,
+		}
+	]);
+
+ 
---
+++ src/components/protObject.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function ProtobjectPanel() {
+  return (
+    <div style={{ display: "flex", gap: "2rem" }}>
+      {/* Controlador: index.html */}
+      <iframe
+        title="Controlador"
+        src="index.html"
+        width="400"
+        height="300"
+        style={{ border: "1px solid #ccc" }}
+      />
+
+      {/* Arduino receptor: arduino.html */}
+      <iframe
+        title="Arduino"
+        src="arduino.html"
+        width="400"
+        height="300"
+        style={{ border: "1px solid #ccc" }}
+      />
+    </div>
+  );
+}
