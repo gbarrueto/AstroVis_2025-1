7b67444df1b32f5e4e37a6e214a63204fcd1c451
Glitch (infovis-g12-e2) <none>
2025-06-21 02:17:12 +0000

    🎹🍱 Checkpoint ./src/components/IframeManager.jsx:77733200/273 ./src/styles/modalConnectStyle.css:77733200/6 ./src/components/modalConnect.jsx:77733200/90

--- src/components/IframeManager.jsx
+++ src/components/IframeManager.jsx
@@ -28,7 +28,7 @@ const IframeManager = forwardRef((props, ref) => {
         top: "50%",
         left: "50%",
         transform: "translate(-50%, -50%)",
-        border: "1px solid #ccc",
+        border: "0px",
         opacity: 0,
         pointerEvents: "none",
         transition: "opacity 0.3s",
